377b8d5def0a2b8f19530368ed331874752cce18
scripthunter7 <d.tota@adguard.com>
2025-12-23 12:09:42 +0100

    Update tsurlfilter reference branch from fix/AG-49044 to fix/AG-42166-5.3

--- bamboo-specs/scripts/link-tsurlfilter.sh
+++ bamboo-specs/scripts/link-tsurlfilter.sh
@@ -44,7 +44,7 @@ done
 #   TSURLFILTER_REF="v2.1.0"           # tag
 #   TSURLFILTER_REF=""                 # skip cloning
 
-TSURLFILTER_REF="fix/AG-49044"
+TSURLFILTER_REF="fix/AG-42166-5.3"
 
 # Repository URLs
 # This should be set as a Bamboo project variable: tsurlfilterRepoUrl
